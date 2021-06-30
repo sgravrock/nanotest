4d83499813529fc6951f4b9e2d5c29d2d691b619
Steve Gravrock <sdg@panix.com>
2021-06-30 14:27:52 -0700

    Split tests and setup boilerplate into separate files

--- self-test.js
+++ self-test.js
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import fs from 'fs';
 import Runner from './runner.js';
+import runnerTests from './runner.test.js';
 
 // Run our own tests using a copy of runner.js that hopefully isn't broken.
 // It'll be copied from runner.js the first time the tests are run and can
@@ -11,114 +12,5 @@ if (!fs.existsSync('known-good-runner.js')) {
 
 const KnownGoodRunner = (await import('./known-good-runner.js')).default;
 const outerRunner = new KnownGoodRunner();
-
-outerRunner.test('#test regisers a test', () => {
-	const subject = new Runner(new MockLogger());
-	function f1() {}
-	function f2() {}
-
-	subject.test('f1', f1);
-	subject.test('f2', f2);
-
-	assert.deepStrictEqual(subject.tests, [
-		{name: 'f1', fn: f1},
-		{name: 'f2', fn: f2},
-	]);
-});
-
-outerRunner.test('#run runs each test', async () => {
-	const subject = new Runner(new MockLogger());
-	let f1Called = false, f2Called = false;
-	function f1() { f1Called = true; }
-	function f2() { f2Called = true; }
-	subject.test('f1', f1);
-	subject.test('f2', f2);
-
-	await subject.run();
-
-	assert(f1Called);
-	assert(f2Called);
-});
-
-outerRunner.test('#run reports success when a test does not throw', async () => {
-	const logger = new MockLogger();
-	const subject = new Runner(logger);
-	subject.test('a test', () => {});
-
-	await subject.run();
-
-	assert.strictEqual(logger.calls.log[0], 'PASS: a test');
-});
-
-outerRunner.test('#run reports failure when a test throws', async () => {
-	const logger = new MockLogger();
-	const subject = new Runner(logger);
-	const error = new Error('nope');
-	subject.test('a test', () => {throw error;});
-
-	await subject.run();
-
-	assert.strictEqual(logger.calls.log[0], 'FAIL: a test');
-	assert.strictEqual(logger.calls.error[0], error);
-});
-
-outerRunner.test('#run waits for an async test to finish before reporting', async () => {
-	const logger = new MockLogger();
-	const subject = new Runner(logger);
-	let testResolve;
-	const testPromise = new Promise(res => testResolve = res);
-	async function f() {
-		await testPromise;
-	}
-	subject.test('test', f);
-
-	subject.run();
-	assert.deepStrictEqual(logger.calls.log, []);
-
-	testResolve();
-	await new Promise(res => setTimeout(res));
-	assert.deepStrictEqual(logger.calls.log, ['PASS: test']);
-});
-
-outerRunner.test('#run waits for an async test to finish before moving on to the next', async () => {
-	const logger = new MockLogger();
-	const subject = new Runner(logger);
-	let f1Called = false, f2Called = false;
-	let f1Resolve;
-	const f1Promise = new Promise(res => f1Resolve = res);
-	async function f1() {
-		f1Called = true;
-		return f1Promise;
-	}
-	function f2() { f2Called = true; }
-	subject.test('f1', f1);
-	subject.test('f2', f2);
-
-	subject.run();
-	assert(f1Called);
-	assert(!f2Called);
-
-	f1Resolve();
-	await new Promise(res => setTimeout(res));
-	assert(f2Called);
-});
-
-
-class MockLogger {
-	constructor() {
-		this.calls = {
-			log: [],
-			error: []
-		};
-	}
-
-	log(msg) {
-		this.calls.log.push(msg);
-	}
-
-	error(e) {
-		this.calls.error.push(e);
-	}
-}
-
+runnerTests(outerRunner);
 outerRunner.run();
