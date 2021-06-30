1e835bf83db90c4f3e0b79772c8d03f6c243e5e6
Steve Gravrock <sdg@panix.com>
2021-06-30 14:27:53 -0700

    Time out if a test does not complete in 1 second

--- runner.js
+++ runner.js
@@ -1,7 +1,10 @@
 export default class Runner {
 
-	constructor(logger) {
-		this.logger = logger || console;
+	constructor(deps) {
+		deps = deps || {};
+		this._logger = deps.logger || console;
+		this._setTimeout = deps.setTimeout || setTimeout;
+		this._clearTimeout = deps.clearTimeout || clearTimeout;
 		this.tests = [];
 	}
 
@@ -16,14 +19,29 @@ export default class Runner {
 	}
 
 	async _runOne(test) {
+		const timeoutToken = {};
+		let timeoutId;
+		const timeoutPromise = new Promise(resolve => {
+			timeoutId = this._setTimeout(function() {
+				resolve(timeoutToken);
+			}, 1000);
+		});
+		let token;
+
 		try {
-			await test.fn();
+			token = await Promise.race([test.fn(), timeoutPromise]);
 		} catch (e) {
-			this.logger.log('FAIL: ' + test.name);
-			this.logger.error(e);
+			this._clearTimeout(timeoutId);
+			this._logger.log('FAIL: ' + test.name);
+			this._logger.error(e);
 			return;
 		}
 
-		this.logger.log('PASS: ' + test.name);
+		if (token === timeoutToken) {
+			this._logger.log('FAIL: ' + test.name + ' (timed out)');
+		} else {
+			this._clearTimeout(timeoutId);
+			this._logger.log('PASS: ' + test.name);
+		}
 	}
 }
--- runner.test.js
+++ runner.test.js
@@ -3,7 +3,7 @@ import Runner from './runner.js';
 
 export default function(outerRunner) {
 	outerRunner.test('#test regisers a test', () => {
-		const subject = new Runner(new MockLogger());
+		const subject = new Runner({logger: new MockLogger()});
 		function f1() {}
 		function f2() {}
 	
@@ -17,7 +17,7 @@ export default function(outerRunner) {
 	});
 	
 	outerRunner.test('#run runs each test', async () => {
-		const subject = new Runner(new MockLogger());
+		const subject = new Runner({logger: new MockLogger()});
 		let f1Called = false, f2Called = false;
 		function f1() { f1Called = true; }
 		function f2() { f2Called = true; }
@@ -32,7 +32,7 @@ export default function(outerRunner) {
 	
 	outerRunner.test('#run reports success when a test does not throw', async () => {
 		const logger = new MockLogger();
-		const subject = new Runner(logger);
+		const subject = new Runner({logger});
 		subject.test('a test', () => {});
 	
 		await subject.run();
@@ -42,7 +42,7 @@ export default function(outerRunner) {
 	
 	outerRunner.test('#run reports failure when a test throws', async () => {
 		const logger = new MockLogger();
-		const subject = new Runner(logger);
+		const subject = new Runner({logger});
 		const error = new Error('nope');
 		subject.test('a test', () => {throw error;});
 	
@@ -54,7 +54,7 @@ export default function(outerRunner) {
 	
 	outerRunner.test('#run waits for an async test to finish before reporting', async () => {
 		const logger = new MockLogger();
-		const subject = new Runner(logger);
+		const subject = new Runner({logger});
 		let testResolve;
 		const testPromise = new Promise(res => testResolve = res);
 		async function f() {
@@ -72,7 +72,7 @@ export default function(outerRunner) {
 	
 	outerRunner.test('#run waits for an async test to finish before moving on to the next', async () => {
 		const logger = new MockLogger();
-		const subject = new Runner(logger);
+		const subject = new Runner({logger});
 		let f1Called = false, f2Called = false;
 		let f1Resolve;
 		const f1Promise = new Promise(res => f1Resolve = res);
@@ -91,6 +91,91 @@ export default function(outerRunner) {
 		f1Resolve();
 		await new Promise(res => setTimeout(res));
 		assert(f2Called);
+	});
+
+	outerRunner.test('#run fails an async test that does not complete within one second', async () => {
+		const logger = new MockLogger();
+		const setTimeoutCalls = [];
+		const subject = new Runner({
+			logger,
+			setTimeout: (fn, delayMs) => {
+				setTimeoutCalls.push({fn, delayMs});
+			}
+		});
+		subject.test('f1', function() {return new Promise(() =>{})});
+		subject.test('f2', function() {});
+
+		const runPromise = subject.run();
+		assert.equal(setTimeoutCalls.length, 1);
+		assert.equal(setTimeoutCalls[0].delayMs, 1000);
+		assert.deepStrictEqual(logger.calls.log, []);
+		assert.deepStrictEqual(logger.calls.error, []);
+
+		assert.equal(setTimeoutCalls.length, 1);
+		setTimeoutCalls[0].fn();
+		await runPromise;
+		assert.deepStrictEqual(logger.calls.log, [
+			'FAIL: f1 (timed out)',
+			'PASS: f2'
+		]);
+	});
+
+	outerRunner.test('#run clears the timeout when a test passes', async () => {
+		const logger = new MockLogger();
+		const setTimeoutCalls = [];
+		const clearTimeoutCalls = [];
+		let lastTimeoutId = 0;
+		const subject = new Runner({
+			logger,
+			setTimeout: (fn, delayMs) => {
+				const id = ++lastTimeoutId;
+				setTimeoutCalls.push({fn, delayMs, id});
+				return id;
+			},
+			clearTimeout: id => {
+				clearTimeoutCalls.push(id);
+			}
+		});
+		let resolveF1;
+		subject.test('f1', function() {
+			return new Promise(resolve => resolveF1 = resolve);
+		});
+
+		const runPromise = subject.run();
+		assert.deepStrictEqual(clearTimeoutCalls, []);
+
+		resolveF1();
+		await runPromise;
+		assert.deepStrictEqual(clearTimeoutCalls, [setTimeoutCalls[0].id]);
+	});
+
+	outerRunner.test('#run clears the timeout when a test fails', async () => {
+		const logger = new MockLogger();
+		const setTimeoutCalls = [];
+		const clearTimeoutCalls = [];
+		let lastTimeoutId = 0;
+		const subject = new Runner({
+			logger,
+			setTimeout: (fn, delayMs) => {
+				const id = ++lastTimeoutId;
+				setTimeoutCalls.push({fn, delayMs, id});
+				return id;
+			},
+			clearTimeout: id => {
+				clearTimeoutCalls.push(id);
+			}
+		});
+		let rejectF1;
+		subject.test('f1', function() {
+			return new Promise((resolve, reject) => rejectF1 = reject);
+		});
+
+		const runPromise = subject.run();
+		assert.deepStrictEqual(clearTimeoutCalls, []);
+
+		rejectF1();
+		await runPromise;
+		assert.deepStrictEqual(clearTimeoutCalls, [setTimeoutCalls[0].id]);
 	});
 }
 
